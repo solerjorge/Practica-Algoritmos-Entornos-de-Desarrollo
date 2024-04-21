f5f448c82567f0abb638dba0f02d728e055ea8c5
solerjorge <solerjorge338@gmail.com>
2024-04-21 16:06:41 +0200

    EsPrimo

--- PracticaAlgoritmos/PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/PracticaAlgoritmos/src/util/Algoritmos.java
@@ -40,4 +40,24 @@ public abstract class Algoritmos {
             return resultado;
         }
     }
+    
+    /**
+     * Verifica si un número entero dado es primo o no.
+     * 
+     * @param numero El número entero a verificar.
+     * @return true si el número es primo, false de lo contrario.
+     */
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false; 
+        }
+        
+        for (int i = 2; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                return false; 
+            }
+        }
+        
+        return true;
+    }
 }
